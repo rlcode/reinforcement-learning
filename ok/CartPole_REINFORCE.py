4c055eae63a127d9d4e6f8c46355e430a6cfadf8
Keon Kim <kwk236@gmail.com>
2017-01-29 02:40:03 -0500

    update keras cartpole

--- ok/CartPole_REINFORCE.py
+++ ok/CartPole_REINFORCE.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import numpy as np 
+import numpy as np
 import gym
 
 
@@ -40,17 +40,17 @@ train = tf.train.RMSPropOptimizer(1e-2).minimize(loss)
 def get_action(obs):
     action = out.eval(feed_dict={x: obs})
     if action[0][0] > np.random.random():
-        return 0 
+        return 0
     else:
         return 1
-    
+
 def discounted_reward(r):
     discounted_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(range(0, len(r))):
         running_add = running_add * 0.99 + r[t]
         discounted_r[t] = running_add
-        
+
     return discounted_r
 
 saver = tf.train.Saver()
@@ -70,35 +70,35 @@ for i in range(10000):
         s.append(obs)
         action = get_action(np.array([obs]))
         action_array = np.zeros(2)
-        
+
         if action == 0:
             action_array[0] = 1
         else:
             action_array[1] = 1
-        
+
         a.append(action_array)
         obs, reward, done, _ = env.step(action)
         r.append(reward)
         reward_sum += reward
-        
+
         if done or reward_sum >= 1000:
             reward_avg = reward_avg*0.9 + reward_sum*0.1
             print ("Episode %i finished. Reward is %i. Average Reward is %f" \
-                   %(_epi, reward_sum, reward_avg)) 
-            
+                   %(_epi, reward_sum, reward_avg))
+
             obs = env.reset()
             reward_sum=0
             _epi += 1
-            
+
             discount_r = discounted_reward(r)
             discount_r -= np.mean(discount_r)
             discount_r /= np.std(discount_r)
-            
+
             if is_train:
                 _, step_result = sess.run([train, out], feed_dict = \
                                           {x: s, act: a, rwd: discount_r})
             s,a,r = [],[],[]
-            
+
             if reward_avg > 800:
                 saver.save(sess, "../tmp/cartpole.ckpt")
                 is_train = False
