275bb1ae79e968db7bc2b5202a891b96871466e5
Keon Kim <kwk236@gmail.com>
2017-01-30 18:08:34 -0500

    update

--- ok/Keras_CartPole_DQN.py
+++ ok/Keras_CartPole_DQN.py
@@ -6,25 +6,28 @@ from gym.wrappers import Monitor
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import sgd
 
-episodes = 4000
+episodes = 5000
+
 
 class DQNAgent:
     def __init__(self):
         self.memory = []
         self.gamma = 0.9  # decay rate
-        self.epsilon = 0.2  # 무작위로 활동할 확률
+        self.epsilon = 0.2  # exploration
         self.epsilon_decay = 0.95
+        self.learning_rate = 0.0001
         self._build_model()
 
     def _build_model(self):
-        # Reinforcement Learning - Deep-Q learning
+        # Deep-Q learning Model
         model = Sequential()
         model.add(Dense(160, input_dim=4, activation='relu'))
         model.add(Dense(160, activation='relu'))
         model.add(Dense(160, activation='relu'))
         model.add(Dense(2))
-        model.compile(loss='mse', optimizer='rmsprop')
+        model.compile(loss='mse', optimizer=sgd(lr=self.learning_rate))
         self.model = model
 
     def remember(self, state, action, reward, next_state):
@@ -40,14 +43,15 @@ class DQNAgent:
         batchs = min(batch_size, len(self.memory))
         batchs = np.random.choice(len(self.memory), batch_size)
         for i in batchs:
-            state_old, action, reward, state = self.memory[i]
+            state, action, reward, next_state = self.memory[i]
             target = reward
             if i != len(self.memory) - 1:
                 target = reward + self.gamma * \
-                         np.amax(self.model.predict(state)[0])
-            target_f = self.model.predict(state_old)
+                         np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
             target_f[0][action] = target
-            self.model.fit(state_old, target_f, nb_epoch=1, verbose=0)
+            print(state, target_f)
+            self.model.fit(state, target_f, nb_epoch=1, verbose=0)
         self.epsilon -= self.epsilon_decay
 
     def load(self, name):
@@ -60,6 +64,7 @@ if __name__ == "__main__":
     agent = DQNAgent()
     env = gym.make('CartPole-v0')
     env = Monitor(env, '/tmp/cartpole-experiment-1', force=True)
+    agent.load("./save/cp-v0.keras")
 
     for e in range(episodes):
         state = env.reset()
@@ -67,15 +72,15 @@ if __name__ == "__main__":
         for time_t in range(5000):
             env.render()
             action = agent.act(state)
-            state_old = state
-            state, reward, done, _ = env.step(action)
-            state = np.reshape(state, [1, 4])
-            agent.remember(state_old, action, reward, state)
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.reshape(next_state, [1, 4])
+            agent.remember(state, action, reward, next_state)
+            state = next_state
             if done:
                 print("episode: {}/{}, score: {}".format(e, episodes, time_t))
                 break
-        if e % 10 == 0:
-            agent.save("./save/cp-v0.keras")
-        if e % 500 == 0:
-            agent.save("./save/cp_backup"+str(e)+"-v0.keras")
-        agent.replay(128)
+        # if e % 10 == 0:
+            # agent.save("./save/cp-v0.keras")
+        # if e % 500 == 0:
+            # agent.save("./save/cp_backup"+str(e)+"-v0.keras")
+        agent.replay(256)
