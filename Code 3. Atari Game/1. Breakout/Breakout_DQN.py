25a8c2288b579e99b972a6b1b5fc34750ebcb4a4
Woongwon <dnddnjs11@naver.com>
2017-04-18 19:43:10 +0900

    add directories about saving graph and save_model and change some code in Breakout_DQN

--- Code 3. Atari Game/1. Breakout/Breakout_DQN.py
+++ Code 3. Atari Game/1. Breakout/Breakout_DQN.py
@@ -2,36 +2,41 @@ import gym
 import pylab
 import random
 import numpy as np
-from skimage.transform import resize
 from collections import deque
-from keras.layers import Dense, Flatten
-from keras.optimizers import Adam
+from skimage.color import rgb2gray
+from skimage.transform import resize
+
 from keras.models import Sequential
+from keras.optimizers import RMSprop
+from keras.layers import Dense, Flatten
 from keras.layers.convolutional import Conv2D
 
 EPISODES = 5000
 
 
 class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.render = False
+    def __init__(self):
+        self.render = True
 
-        self.state_size = state_size
-        self.action_size = action_size
+        self.state_size = (84, 84, 4)
+        self.action_size = 6
 
         self.epsilon = 1.0
         self.epsilon_start = 1.0
         self.epsilon_end = 0.1
-        self.epsilon_decay = 1000000
+        self.epsilon_decay = 1000000.
         self.epsilon_decay_step = \
             (self.epsilon_start - self.epsilon_end) / self.epsilon_decay
 
         self.batch_size = 32
-        self.train_start = 50000
+        self.train_start = 20000
         self.update_target_rate = 10000
         self.discount_factor = 0.99
+        self.memory = deque(maxlen=400000)
+        self.no_op_steps = 30
         self.learning_rate = 0.00025
-        self.memory = deque(maxlen=1000000)
+        self.momentum = 0.95
+        self.min_gradient = 0.01
 
         self.model = self.build_model()
         self.target_model = self.build_model()
@@ -47,49 +52,51 @@ class DQNAgent:
                          kernel_initializer='glorot_uniform'))
         model.add(Flatten())
         model.add(Dense(512, activation='relu', kernel_initializer='glorot_uniform'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(lr=0.00025, beta_1=0.95, beta_2=0.95, epsilon=0.01))
+        model.add(Dense(self.action_size))
+        model.summary()
+        model.compile(loss='mse', optimizer=RMSprop(
+            lr=self.learning_rate, rho=self.momentum, epsilon=self.min_gradient))
         return model
 
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
     def get_action(self, history):
+        history = np.float32(history/255.0)
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
-            q_value = self.model.predict(np.reshape(np.float32(history[:, :, 0:4] / 255.), [1, 84, 84, 4]))
+            q_value = self.model.predict(history)
             return np.argmax(q_value[0])
 
-    def replay_memory(self, history, action, reward, done):
-        self.memory.append((history, action, reward, done))
+    def replay_memory(self, history, action, reward, history1, done):
+        self.memory.append((history, action, reward, history1, done))
         if self.epsilon > self.epsilon_end:
             self.epsilon -= self.epsilon_decay_step
-        # print(len(self.memory))
 
     def train_replay(self):
-        # if len(self.memory) < self.train_start:
-        #     return
+        if len(self.memory) < self.train_start:
+            return
         batch_size = min(self.batch_size, len(self.memory))
         mini_batch = random.sample(self.memory, batch_size)
 
-        update_input = []
-        update_target = []
+        update_input = np.zeros((batch_size, self.state_size[0], self.state_size[1], self.state_size[2]))
+        update_target = np.zeros((batch_size, self.action_size))
 
         for i in range(batch_size):
-            history, action, reward, done = mini_batch[i]
+            history, action, reward, history1, done = mini_batch[i]
             history = np.float32(history/255.)
-            target = self.model.predict(np.reshape(history[:, :, 0:4], [1, 84, 84, 4]))[0]
+            history1 = np.float32(history1/255.)
+            target = self.model.predict(history)[0]
 
             if done:
                 target[action] = reward
             else:
-                target[action] = reward + self.discount_factor * np.amax(self.target_model.predict
-                                        (np.reshape(history[:, :, 1:5], [1, 84, 84, 4]))[0])
-            update_target.append(target)
-            update_input.append(np.reshape(history[:, :, 0:4], [84, 84, 4]))
+                target[action] = reward + self.discount_factor * np.amax(self.target_model.predict(history1)[0])
+            update_target[i] = target
+            update_input[i] = history
 
-        self.model.fit(np.array(update_input), np.array(update_target), batch_size=batch_size, epochs=1, verbose=0)
+        self.model.fit(update_input, update_target, batch_size=batch_size, epochs=1, verbose=0)
 
     def load_model(self, name):
         self.model.load_weights(name)
@@ -98,52 +105,57 @@ class DQNAgent:
         self.model.save_weights(name)
 
 
-def pre_processing(observe):
-    observe = np.uint8(np.dot(observe[:, :, :3], [0.299, 0.587, 0.114]))
-    print(observe[30, :])
-    observe = resize(observe, (110, 84), mode='reflect')
-    print(observe[30, :])
-    observe = observe[17:101, :]
-    return observe
+def pre_processing(next_observe, observe):
+    processed_observe = np.maximum(next_observe, observe)
+    processed_observe = np.uint8(resize(rgb2gray(processed_observe), (84, 84), mode='constant')*255)
+    return processed_observe
 
 
 if __name__ == "__main__":
     env = gym.make('BreakoutDeterministic-v3')
-    env.seed(0)
-    state_size = (84, 84, 4)
-    action_size = env.action_space.n
-
-    agent = DQNAgent(state_size, action_size)
+    agent = DQNAgent()
 
     scores, episodes, global_step = [], [], 0
-    history = np.zeros([84, 84, 5], dtype=np.uint8)
 
     for e in range(EPISODES):
         done = False
+        dead = False
         score, start_live = 0, 5
         observe = env.reset()
-        state = pre_processing(observe)
-        for i in range(4):
-            history[:, :, i] = state
+        next_observe = observe
+        for _ in range(random.randint(1, agent.no_op_steps)):
+            observe = next_observe
+            next_observe, _, _, _ = env.step(1)
+
+        state = pre_processing(next_observe, observe)
+        history = np.stack((state, state, state, state), axis=2)
+        history = history.reshape(1, history.shape[0], history.shape[1], history.shape[2])
 
         while not done:
             if agent.render:
                 env.render()
+            observe = next_observe
             action = agent.get_action(history)
             next_observe, reward, done, info = env.step(action)
-            # next_state = pre_processing(next_observe)
+            next_state = pre_processing(next_observe, observe)
+            next_state = np.reshape([next_state], (1, 84, 84, 1))
+            history1 = np.append(next_state, history[:, :, :, :3], axis=3)
+
             if start_live > info['ale.lives']:
-                done = True
+                dead = True
                 start_live = info['ale.lives']
-                reward = -1
 
-            history[:, :, 4] = next_state
-
-            agent.replay_memory(history[:, :, 0:5], action, reward, done)
+            agent.replay_memory(history, action, reward, history1, done)
             agent.train_replay()
 
-            history[:, :, 0:4] = history[:, :, 1:5]
             score += reward
+
+            if dead:
+                history = np.stack((next_state, next_state, next_state, next_state), axis=2)
+                history = np.reshape([history], (1, 84, 84, 4))
+                dead = False
+            else:
+                history = history1
 
             if global_step % agent.update_target_rate == 0:
                 agent.update_target_model()
@@ -153,6 +165,10 @@ if __name__ == "__main__":
                 scores.append(score)
                 episodes.append(e)
                 pylab.plot(episodes, scores, 'b')
-                pylab.savefig("./save_graph/Breakout_DQN1.png")
+                pylab.savefig("./save_graph/Breakout_DQN.png")
                 print("episode:", e, "  score:", score, "  memory length:", len(agent.memory),
                       "  epsilon:", agent.epsilon)
+
+        # 20 에피소드마다 학습 모델을 저장
+        if e % 1000 == 0:
+            agent.save_model("./save_model/Breakout_DQN.h5")
