d0c8a74fdfa0d30d053f5b7869a35588456bf245
juice100 <julienlook@gmx.de>
2020-02-09 05:55:22 +0100

    test git on windows

--- cartpole/A2C_cart/cartpole_a2c.py
+++ cartpole/A2C_cart/cartpole_a2c.py
@@ -8,7 +8,7 @@ import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import Adam
-
+import trina
 import time
 timestr = time.strftime("%d.%m.%Y - %H:%M:%S")
 
